a06c0e9c381c2212e1ae138328863fa6ad341066
mohamedesmat1997 <158211063+63456356@users.noreply.github.com>
2024-05-31 12:22:51 +0300

    working hours front

--- src/app/navigate/navigate.component.ts
+++ src/app/navigate/navigate.component.ts
@@ -4,11 +4,12 @@ import { OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-navigate',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [HttpClientModule , FormsModule],
   templateUrl: './navigate.component.html',
   styleUrl: './navigate.component.css'
 })
@@ -23,23 +24,32 @@ export class NavigateComponent implements OnInit {
       hub:any;
       data:any=[];
       info:any=[];
+      working:any=[];
+      hours:any=[];
       role:any='';
+      patientid:any='';
+      doctorid:any='';
+      username:any='';
       httpCliet=inject(HttpClient)
     
       ngOnInit() {
         this.role=localStorage.getItem('role')
+        this.patientid=localStorage.getItem('user_id')
+        this.username=localStorage.getItem('username')
 
-      const id=this.activatedRoute.snapshot.params['id'];
-    console.log(id); 
+     this.doctorid=this.activatedRoute.snapshot.params['id'];
+    console.log(this.doctorid); 
 
-    this.httpCliet.get(`http://localhost:1999/doctor/navi/${id}`).subscribe(  (res:any)=>{ 
+    this.httpCliet.get(`http://localhost:1999/doctor/navi/${this.doctorid}`).subscribe(  (res:any)=>{ 
       this.data=res
       this.info=this.data[0]
       console.log(this.data[0]); 
     });
 
-    this.httpCliet.get(`http://localhost:1999/workinghours/${id}`).subscribe(  (res:any)=>{ 
-      console.log(res); 
+    this.httpCliet.get(`http://localhost:1999/workinghours/${this.doctorid}`).subscribe(  (res:any)=>{ 
+      this.working=res
+      this.hours=this.working[0]
+      console.log(this.hours); 
     });
 
       }
